2dc43eeb1691b4a2a05d297167bf34995ab981ef
Jas Kuner <jas.kuner@snyk.io>
2022-08-11 17:28:02 +0100

    spike: add makeAsyncRequest

--- src/lib/snyk-test/run-test.ts
+++ src/lib/snyk-test/run-test.ts
@@ -226,6 +226,7 @@ async function sendAndParseResults(
     /** sendTestPayload() deletes the request.body from the payload once completed. */
     const payloadCopy = Object.assign({}, payload);
     const res = await sendTestPayload(payload);
+    console.log('++++++++++res', JSON.stringify(res));
     const {
       depGraph,
       payloadPolicy,
@@ -282,6 +283,7 @@ export async function runTest(
   root: string,
   options: Options & TestOptions,
 ): Promise<TestResult[]> {
+  console.log('++++HERE+++++');
   const spinnerLbl = 'Querying vulnerabilities database...';
   try {
     const payloads = await assemblePayloads(root, options);
